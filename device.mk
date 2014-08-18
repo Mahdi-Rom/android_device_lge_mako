a6145b083e2433fda32a51edc80d1f26aae82200
Mahdi-Rom <mahdi.rom@gmail.com>
2014-08-18 02:31:32 +0100

    Build TouchControl package

--- device.mk
+++ device.mk
@@ -260,5 +260,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
 
+# TouchControl package.
+PRODUCT_PACKAGES += \
+	TouchControl
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
